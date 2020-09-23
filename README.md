996391519264b323c908537a632ed06ca152b710
baba_s <shotababa1206@gmail.com>
2020-09-23 20:08:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UniIL2CPPChecker
---
+++ Scripts/IL2CPPChecker.cs
@@ -0,0 +1,17 @@
+﻿namespace Kogane
+{
+	public static class IL2CPPChecker
+	{
+		public static bool IsIL2CPP
+		{
+			get
+			{
+#if ENABLE_IL2CPP
+				return true;
+#else
+				return false;
+#endif
+			}
+		}
+	}
+}
